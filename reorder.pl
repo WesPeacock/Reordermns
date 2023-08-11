a289ff2e0723a4782f69c2de40657b9726bdca9d
Wes <wes_peacock@sil.org>
2023-08-11 09:30:00 -0600

    Basic proof of concept

---
+++ reorder.pl
@@ -0,0 +1,37 @@
+use String::Approx qw{aindex amatch};
+use v5.20;
+=pod
+my $lx= $1 if /\\lx ([^#]*)/;
+if (/(\\mn[^#]*#)+/) {
+	say "lx:$lx";
+	my $mns=$MATCH;
+	$mns =~ s/\\mn //g;
+	my @a = split (/#/, $mns);
+	foreach ( @a) {
+		say ">$_<";
+		}
+	}
+=cut
+my $lx; my @mns; my @fuzinds; my $debug=1;
+$lx = 'living on the wild side';
+@mns = split /#/, 'live#side#wild';
+$lx = 'iyikxi-a̱kayo';
+@mns = split /#/, '-ikxi#a̱ka';
+$lx = 'now was the time for all good men too come too the aid ofg the party';
+@mns = qw /now was the time for all good men too come too the aid off the party/;
+$lx = 'atowunhɛ';
+@mns = split /#/, '-hɛ#wun#atɔ';
+say "@mns";
+say "=====";
+foreach (@mns) {
+	push @fuzinds, aindex ($_, $lx);
+	}
+say STDERR "@fuzinds" if $debug;
+# H/T https://stackoverflow.com/a/16397775/1170224
+#my @idx = sort { $fuzinds[$a] <=> $fuzinds [$b] } 0 .. $#fuzinds;
+my @idx = sort {  aindex ($mns[$a], $lx) <=> aindex ($mns[$b], $lx) } 0 .. $#mns;
+@mns = @mns[@idx];
+say $lx;
+foreach (@mns) {
+	say $_;
+	}
